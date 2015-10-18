503831daa00d84dea812b35a627d4b43faf98e25
blinzelaffe <m.drobisch@googlemail.com>
2015-10-18 14:14:33 +0200

    feature(core): add rfid-detect to the background-worker

--- server/app/worker.py
+++ server/app/worker.py
@@ -5,7 +5,7 @@ from server import app
 import threading
 import os
 
-from RFID import RFID
+from RFID import RFIDReader
 from RFID import RFIDMockup
 
 from GPIO import GPIO
@@ -23,11 +23,45 @@ class BackgroundWorker():
         GPIO.setup(12,GPIO.OUT, initial=GPIO.HIGH)
         GPIO.output(12, GPIO.HIGH)
 
+
     def run(self):
         if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
             self.thr = threading.Timer(1, self.timer_cycle)
             self.thr.start()
             print 'started background-server'
+
+    def readRFIDTag(self):
+        print "try rfid detect"
+        (status,TagType) = RFIDReader.MFRC522_Request(RFIDReader.PICC_REQIDL)
+
+        # If a card is found
+        if status == RFIDReader.MI_OK:
+            print "Card detected"
+
+        # Get the UID of the card
+        (status,uid) = RFIDReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == RFIDReader.MI_OK:
+
+            # Print UID
+            print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+
+            # This is the default key for authentication
+            key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+
+            # Select the scanned tag
+            RFIDReader.MFRC522_SelectTag(uid)
+
+            # Authenticate
+            status = RFIDReader.MFRC522_Auth(RFIDReader.PICC_AUTHENT1A, 8, key, uid)
+
+            # Check if authenticated
+            if status == RFIDReader.MI_OK:
+                RFIDReader.MFRC522_Read(8)
+                RFIDReader.MFRC522_StopCrypto1()
+            else:
+                print "Authentication error"
 
     def timer_cycle(self):
         self.thr = threading.Timer(1, BackgroundWorker.timer_cycle,[self])
@@ -45,6 +79,8 @@ class BackgroundWorker():
                 self.openingTimer = -1
                 print "Closing door"
                 GPIO.output(12, GPIO.HIGH)
+
+        self.readRFIDTag()
 
         #else:
             #print "Closing door"
