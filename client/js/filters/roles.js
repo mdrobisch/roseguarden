fdd89774cecbcec954cd792a7455f626772b5553
blinzelaffe <m.drobisch@googlemail.com>
2016-02-02 21:58:54 +0100

    ADD (frontend) role of user in the profile view with angular-filter

--- client/js/filters/roles.js
+++ client/js/filters/roles.js
@@ -1,3 +1,20 @@
 /**
  * Created by drobisch on 02.02.16.
  */
+RoseGuardenApp.filter('rolesFilter', function () {
+    return function (type) {
+        switch(type)
+        {
+            case 0:
+                return "User";
+            case 1:
+                return "Admin"
+            case 2:
+                return "Superuser"
+            default:
+                return "Invalid role";
+        }
+        return "None";
+        // return firstNumber % secondNumber > 0
+    };
+});
