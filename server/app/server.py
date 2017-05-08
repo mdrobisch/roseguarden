9dd1bd1583b0977b6151b803f9c5e53aaae964f9
mdrobisch <m.drobisch@googlemail.com>
2017-05-08 22:24:08 +0200

    FIX (backend) caching problems after setup
    disable caching for now

--- server/app/server.py
+++ server/app/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
@@ -7,17 +8,20 @@ from flask_alchemydumps import AlchemyDumps
 
 from flask_httpauth import HTTPBasicAuth
 from flask_mail import Mail
-from extension import extension_name
+from extension import ExtensionManager
+from config import ConfigManager
+import datetime
 
-app = Flask(__name__, static_url_path='', static_folder='../extensions/' + extension_name + '/frontend')
+app = Flask(__name__, static_url_path='', static_folder='../extensions/' + ExtensionManager.extension_name + '/frontend')
 # have a look at
 # http://stackoverflow.com/questions/26722279/how-to-set-static-url-path-in-flask-application
 # http://flask.pocoo.org/snippets/102/
-
 flask_bcrypt = Bcrypt(app)
-app.config.from_object('app.config')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/roseGuarden.db'
 app.config['SQLALCHEMY_MIGRATE_DATABASE_URI'] = 'sqlite:///database/roseGuarden.db'
+app.config['WTF_CSRF_ENABLED'] = ConfigManager.WTF_CSRF_ENABLED
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = ConfigManager.SQLALCHEMY_TRACK_MODIFICATIONS
+app.config['DEBUG'] = ConfigManager.DEBUG
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -33,6 +37,13 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+
+    # cahce
+
+    response.headers['Last-Modified'] = datetime.datetime.now()
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, post-check=0, pre-check=0, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "-1"
     return response
 
 @app.route('/')
